6a009f035b5a5f807e9a2fc064f1493e2e459f9e
Talha Hussain <talhahussain056@gmail.com>
2024-08-04 22:27:59 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+/*
+  Dart Object Oriented Programming 
+
+Object Oriented Programming is Computer Programming model that organize software design around data or objects rather than functions and logic.
+
+An object can be defined as data  field
+that has unique attributes and behaivour.
+
+Advantages OOP : 
+Increase the useability and decrease the complexity ,increase the prodictivity of programmer
+Helps us debug code and write code efficently.  
+
+Null Safety in Dart 
+
+Non -nullable variable : Those variable that must contain a value and cannot be nil.
+Nullable Variable means we will assign value to the variable later
+In order to make a variable nullable we do this 
+int ? age ;
+print(age); here the age is nullable
+
+
+
+
+*/
